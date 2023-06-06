de40c165740bc46e0c79ac4a75d7ca4498bdbf43
Burrouwill <wgr.burroughs@gmail.com>
2023-06-06 18:44:24 +1200

    KMP algorithm working, added some addittional failsafes to handle user giving invalid args

--- src/KMP.java
+++ src/KMP.java
@@ -13,7 +13,82 @@ public class KMP {
 	 * exists, or -1 if it doesn't.
 	 */
 	public static int search(String pattern, String text) {
-		// TODO fill this in.
-		return -1;
+		try {
+			// Handle IllegalArgs
+			if (pattern == null || text == null) {
+				throw new IllegalArgumentException("Text and/or Pattern is null.");
+			}
+			if (pattern.length() > text.length()) {
+				throw new IllegalArgumentException("Pattern longer than Text.");
+			}
+			if (pattern.equals("") || text.equals("")) {
+				throw new IllegalArgumentException("Empty Pattern and/or Text.");
+			}
+
+			// Calc Jump Table
+			int[] prefixLength = calcJumpTable(pattern);
+
+			// Initialise variables to be used
+			int tLength = text.length();
+			int pLength = pattern.length();
+			int t = 0;
+			int p = 0;
+
+			// Start search through text
+			while (t < tLength) {
+				if (pattern.charAt(p) == text.charAt(t)) {
+					p++;
+					t++;
+
+					if (p == pLength) { // If we have matched entire pattern --> Return starting index
+						return t - p;
+					}
+
+				} else { // If we fail to match a char
+					p = prefixLength[p]; // Set p to jump value
+
+					if (p < 0) { // Handles the first value from Jump Table (-1)
+						t++;
+						p++;
+					}
+				}
+			}
+		} catch (IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+		}
+		return -1; // We couldnt find it
 	}
+
+	/**
+	 * Calculates the Jump Table for the pattern, to be used in the KMP main
+	 * algorithm.
+	 * 
+	 * @param pattern
+	 * @return
+	 * @author wgrbu
+	 */
+	public static int[] calcJumpTable(String pattern) {
+
+		int patternLength = pattern.length();
+		int[] jumpTable = new int[patternLength + 1];
+		jumpTable[0] = -1; // Set special case 1st value of jump table
+
+		int prefixLength = 0;
+		int i = 1;
+
+		while (i < patternLength) {
+			if (pattern.charAt(prefixLength) == pattern.charAt(i)) {
+				prefixLength++;
+				i++;
+				jumpTable[i] = prefixLength;
+			} else if (prefixLength > 0) {//
+				prefixLength = jumpTable[prefixLength];
+			} else { // Prefix length 0 so set that in array and move on one char
+				i++;
+				jumpTable[i] = 0;
+			}
+		}
+		return jumpTable;
+	}
+
 }
